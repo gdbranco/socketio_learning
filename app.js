b6d3a17d3443409df9b1df2d1d733227fcc5fd62
gdbranco <gdbranco@gmail.com>
2017-10-24 09:04:19 -0200

    Fix reset button and keep alive

--- app.js
+++ app.js
@@ -33,39 +33,63 @@ app.use(
 app.use(express.static(path.join(__dirname, '/public')));
 //EJS AS PRIMARY VIEW ENGINE SETUP
 app.set("view engine", "ejs");
+Page.remove({},function(error){});
 //===============================================================
 //ROUTING SETUPS
 var indexRoutes = require("./routes/index");
 var errorRoutes = require("./routes/404");
 app.use(indexRoutes);
 app.use(errorRoutes);
+socket_ids = [];
 io.sockets.on('connection', function (socket) {
     var cPage = socket.handshake.headers.referer.split("/");
     cPage = "/" + cPage[cPage.length - 1];
-    Page.find({ page: cPage }, function (error, found) {
-        if (found.length == 0) {
-            Page.create({ page: cPage, count: 1 });
-        }
-        else {
-            found[0].count++;
-            found[0].save();
+    socket.on("keep-alive",function(data){
+        if(socket_ids.indexOf(socket.id)==-1){
+            socket_ids.push(socket.id);
+            Page.find({page: cPage},function(error,found){
+                if(found.length == 0){
+                    Page.create({page: cPage, count: 1});
+                }else{
+                    found[0].count++;
+                    found[0].save();
+                }
+            });
         }
     });
+    socket.on('reset-info', function(data){
+        Page.find({},function(error,all){
+            all.forEach(function(each){
+                each.count = 0;
+                each.save();
+                socket_ids.splice(0,socket_ids.length);
+            });
+        });        
+    });
     socket.on('disconnect', function (data) {
-        // console.log("Disconnect from : " + socket.handshake.headers.referer)
         Page.find({ page: cPage }, function (error, found) {
-            found[0].count--;
-            found[0].save();
+            if(found.length){
+                if(found[0].count>0){
+                    found[0].count--;
+                    found[0].save();
+                    index = socket_ids.indexOf(socket.id);
+                    socket_ids.splice(index,1);
+                }
+            }
         });
     });
     setInterval(function () {
         Page.find({page: cPage}, function (error, found) {
-            socket.emit("visiting-info", found[0].count);
+            if(found.length){
+                socket.emit("visiting-info", found[0].count);
+            }
         });
     },2000);
     setInterval(function () {
         Page.find({count: {$gt: 0}}, function (error, all) {
-            socket.emit("dashboard-info", all);
+            if(all.length){
+                socket.emit("dashboard-info", all);
+            }
         });
     },2000);
 });
--- public/javascript/socketio.js
+++ public/javascript/socketio.js
@@ -1,11 +1,11 @@
 $(function () {
     var socket = io.connect();
-    // var visitorData = {
-    //     currentPage: location.pathname
-    // }
-    // socket.emit('visitor-data',visitorData);
+    setInterval(function(){
+        socket.emit("keep-alive");
+    },2000);
     socket.on('dashboard-info', function(data){
         $("#page_board").html("");
+        data.sort(function(a,b){return a.page > b.page});
         data.forEach(function(page){
             $("#page_board").append(
                 `
@@ -16,5 +16,8 @@ $(function () {
     });
     socket.on('visiting-info',function(data){
         $("#page_visitors").html("Visitantes desta página - " + data);
+    });
+    $("#btn_reset").on('click', function(){
+        socket.emit("reset-info");
     });
 });
